d0c1c8f851cf021eab4f0830bf1ae4410f64a010
Alex Espinoza <aespinoza@aspria.net>
2016-06-25 15:05:02 -0400

    Start fleshing out /establishments/find endpoint

--- rails/app/controllers/api/establishments_controller.rb
+++ rails/app/controllers/api/establishments_controller.rb
@@ -1,5 +1,10 @@
 class Api::EstablishmentsController < ApplicationController
   def find
+    searcher = YelpSearcher.new('42.347152699999995', '-71.1352516', 805);
+    searcher.create_randomized_search_parameters
+    searcher.search
+    establishment = searcher.choose_random_business
+
     render json: {response: 'ok'}
   end
 end
