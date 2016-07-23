e37e065c8bb1a2b32653624513f722f492e3bc87
Alex Espinoza <aespinoza@aspria.net>
2016-07-23 15:18:14 -0400

    EstablishmentsController now handles latitude and longitude passed to it from client

--- app/controllers/api/establishments_controller.rb
+++ app/controllers/api/establishments_controller.rb
@@ -1,18 +1,19 @@
 class Api::EstablishmentsController < ApplicationController
   def find
-    searcher = YelpSearcher.new('42.347152699999995', '-71.1352516', 805);
+    searcher = YelpSearcher.new(params[:latitude], params[:longitude], 805);
     searcher.create_randomized_search_parameters
 
     begin
       searcher.search
     rescue Yelp::Error::Base => e
-      render json: {error: e}
-      return
+      return render json: {error: e},
+                    status: 500
     end
 
     random_establishment = searcher.choose_random_business
     establishment = Establishment.build_for_api_response(random_establishment)
 
-    render json: establishment
+    render json: establishment,
+           status: 200
   end
 end
