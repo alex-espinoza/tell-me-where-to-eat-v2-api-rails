5124766aced809d3f3926f162e5df6c3d9b91694
Alex Espinoza <aespinoza@aspria.net>
2016-07-24 17:43:46 -0400

    Create environment specific options for rack-cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,12 +5,24 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    origins '*'
+if Rails.env.production?
+  Rails.application.config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins 's3.amazonaws.com'
 
-    resource '*',
-      headers: :any,
-      methods: [:get]
+      resource '/api',
+        headers: :any,
+        methods: [:get]
+    end
+  end
+else
+  Rails.application.config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins '*'
+
+      resource '*',
+        headers: :any,
+        methods: [:any]
+    end
   end
 end
