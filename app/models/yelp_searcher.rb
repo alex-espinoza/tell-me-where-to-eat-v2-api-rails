f663501c41959d672f915dc5f47b91477a1a192d
Alex Espinoza <aespinoza@aspria.net>
2018-11-24 12:48:58 -0600

    Update YelpSearcher to use new yelp API v3 parameters

--- app/models/yelp_searcher.rb
+++ app/models/yelp_searcher.rb
@@ -10,20 +10,22 @@ class YelpSearcher
   end
 
   def create_randomized_search_parameters
-    @sort = Random.rand(0..2) # 0 = Best matched, 1 = Distance, 2 = Highest Rated
+    @sort_by = ['best_match', 'rating', 'review_count', 'distance'].sample
     @categories = CategoryList.get_random(20)
   end
 
   def search
     @response = Yelp.client.search_by_coordinates(
-      { latitude: @latitude,
+      {
+        latitude: @latitude,
         longitude: @longitude
       },
-      { term: '',
+      {
+        term: '',
         limit: RESULTS_LIMIT,
-        sort: @sort,
-        category_filter: @categories,
-        radius_filter: @meters
+        sort_by: @sort_by,
+        categories: @categories,
+        radius: @meters
       }
     )
   end
